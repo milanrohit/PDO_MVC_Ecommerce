647bd3ff3213747ce999b79069d0aa21689c6e01
milanrohit <rohit.milan3@gmail.com>
2025-02-11 13:21:48 +0000

    Manage product development

--- backend/manageproduct.php
+++ backend/manageproduct.php
@@ -2,7 +2,6 @@
     include_once("../config/connection.php");
     include_once("../lib/function.inc.php");
     include_once("header.inc.php");
-    include_once("../controller/ProductMasterController.php");
     include_once("../model/ProductMasterModel.php");
     include_once("../model/CategoryMasterModel.php");
 
@@ -92,13 +91,13 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header"><strong>Product Form</strong></div>
-                    <form method="post" action="your_action_page.php" enctype="multipart/form-data">
+                    <form method="POST" action="" enctype="multipart/form-data">
                         <div class="container mt-5">
                             <div class="card">
                                 <div class="card-body card-block">
                                     <div class="form-group">
-                                        <label for="dropdown">Choose an option:</label>
-                                        <select id="dropdown" name="Product_Name" class="form-control" required>
+                                        <label for="dropdown">Product Choose an Categorie:</label>
+                                        <select id="dropdown" name="Product_CategorieId" class="form-control" required>
                                             <?php if (!empty($CategoryMasterDetails) && is_array($CategoryMasterDetails)): ?>
                                                 <?php foreach ($CategoryMasterDetails as $key => $val): ?>
                                                     <option value="<?php echo sanitizeString($val['Categories_Id']); ?>">
@@ -112,19 +111,19 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label for="textbox">Input Text:</label>
-                                        <input type="text" id="textbox" name="textbox" class="form-control" required>
+                                        <label for="textbox">Product Name</label>
+                                        <input type="text" id="Product_Name" name="Product_Name" class="form-control" required>
                                         <div class="invalid-feedback">
                                             Please provide some text.
                                         </div>
                                     </div>
-                                    <div class="form-group">
+                                    <!--<div class="form-group">
                                         <label for="photo">Upload Photo:</label>
-                                        <input type="file" id="photo" name="photo" class="form-control-file" required>
+                                        <input type="file" id="photo" name="Product_Img" class="form-control-file" required>
                                         <div class="invalid-feedback">
                                             Please upload a photo.
                                         </div>
-                                    </div>
+                                    </div>-->
                                     <button name="submit" type="submit" class="btn btn-primary float-right">Submit</button>
                                 </div>
                             </div>
